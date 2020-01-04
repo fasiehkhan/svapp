5a29000cbfa50e2746b0b3a6583efb9929d257fe
Fasieh Khan <fasieh.khan@ryerson.ca>
2020-01-04 18:16:53 -0500

    Updates

---
+++ precache-manifest.226b2a302d004336d9f0fc94c069fef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149cd3980a6c335697e5c817fe3290c9",
+    "url": "/svapp/index.html"
+  },
+  {
+    "revision": "45e7d124fa6b0a959d08",
+    "url": "/svapp/static/css/main.595858a6.chunk.css"
+  },
+  {
+    "revision": "6a30cd99785fd20747cf",
+    "url": "/svapp/static/js/2.386d7c80.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/svapp/static/js/2.386d7c80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45e7d124fa6b0a959d08",
+    "url": "/svapp/static/js/main.4896d545.chunk.js"
+  },
+  {
+    "revision": "1e669c25a39ef48456ab",
+    "url": "/svapp/static/js/runtime-main.8bfe6b9c.js"
+  }
+]);
