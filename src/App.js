d48366e9f41a51125ec3d57433a119855f258d29
Fasieh Khan <56199997+fasiehkhan@users.noreply.github.com>
2020-02-08 14:40:58 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-  
 import React from 'react';
-import SortingVisuacdlizer from './SortingVisualizer/SortingVisualizer';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 import './App.css';
+
+
 function App() {
+
   return (
     <div className="App">
-      <SortingVisualizer></SortingVisualizer>
+      <SortingVisualizer/>
     </div>
   );
+
+  
 }
 
 export default App;
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -3,16 +3,16 @@ import {getMergeSortAnimations} from '../sortingAlgorithms/sortingAlgorithms.js'
 import './SortingVisualizer.css';
 
 // Change this value for the speed of the animations.
-const ANIMATION_SPEED_MS = 1;
+const ANIMATION_SPEED_MS = 9;
 
 // Change this value for the number of bars (value) in the array.
-const NUMBER_OF_ARRAY_BARS = 305;
+const NUMBER_OF_ARRAY_BARS = 43;
 
 // This is the main color of the array bars.
-const PRIMARY_COLOR = 'turquoise';
+const PRIMARY_COLOR = 'black';
 
 // This is the color of array bars that are being compared throughout the animations.
-const SECONDARY_COLOR = 'red';
+const SECONDARY_COLOR = 'pink';
 
 export default class SortingVisualizer extends React.Component {
   constructor(props) {
@@ -30,7 +30,7 @@ export default class SortingVisualizer extends React.Component {
   resetArray() {
     const array = [];
     for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
-      array.push(randomIntFromInterval(5, 730));
+      array.push(randomIntFromInterval(5, 300));
     }
     this.setState({array});
   }
@@ -59,19 +59,6 @@ export default class SortingVisualizer extends React.Component {
     }
   }
 
- 
-  testSortingAlgorithms() {
-    for (let i = 0; i < 100; i++) {
-      const array = [];
-      const length = randomIntFromInterval(1, 1000);
-      for (let i = 0; i < length; i++) {
-        array.push(randomIntFromInterval(-1000, 1000));
-      }
-      const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
-      const mergeSortedArray = getMergeSortAnimations(array.slice());
-      console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));
-    }
-  }
 
   render() {
     const {array} = this.state;
@@ -87,19 +74,16 @@ export default class SortingVisualizer extends React.Component {
               height: `${value}px`,
             }}></div>
         ))}
-        <button onClick={() => this.resetArray()}>Generate New Array</button>
-        <button onClick={() => this.mergeSort()}>Merge Sort</button>
-        <button onClick={() => this.quickSort()}>Quick Sort</button>
-        <button onClick={() => this.heapSort()}>Heap Sort</button>
-        <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
-        <button onClick={() => this.testSortingAlgorithms()}>
-          Test Sorting Algorithms (BROKEN)
-        </button>
+        <button className="genButton"onClick={() => this.resetArray()}>Generate New Array</button>        
+        <button className="mergeButton"onClick={() => this.mergeSort()}>Merge Sort</button>
+
       </div>
     );
   }
+  
 }
 
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
 function randomIntFromInterval(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
